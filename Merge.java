302f728d11f78a5eb315358af0c3797dcdeb0aa4
Jamie Zeng <jzeng3@stuy.edu>
2019-03-15 10:04:26 -0400

    temp arrays works

--- Merge.java
+++ Merge.java
@@ -2,33 +2,37 @@ import java.io.*;
 import java.util.*;
 public class Merge{
   public static void main(String[] args){
-    int[] ary = {0,1,5,3};
+    int[] ary = {0,1,5,32,7,5,3};
 
-    mergesort(ary, 0, 3);
+    mergesort(ary, 0, ary.length - 1);
   }
 
   public static void mergesort(int[]data){
 
   }
   public static void mergesort(int[] data, int lo, int hi){
+    System.out.println("mergesort "+Arrays.toString(data)+", "+lo+", "+hi);
     if (lo >= hi){
       return;
     }
     int mid = (lo + hi) / 2;
     System.out.println("mid: "+mid);
     int[] tempLeft = new int[mid - lo+1];
-    int[] tempRight = new int[hi - mid -1];
-    System.out.println("temp length: "+tempLeft.length);
+    int[] tempRight = new int[hi - mid];
+    System.out.println("tempLeft length: "+tempLeft.length);
+    System.out.println("tempRight length: "+tempRight.length);
     for (int i = 0; i < tempLeft.length; i++){
-      tempLeft[i] = data[i];
+      tempLeft[i] = data[lo+i];
     }
     for (int i = 0; i < tempRight.length; i++){
+      int j = i+mid+1;
+      System.out.println("i+mid+1: "+j);
       tempRight[i] = data[i+mid+1];
     }
     System.out.println(Arrays.toString(tempLeft));
     System.out.println(Arrays.toString(tempRight));
-    mergesort(tempLeft, lo, mid);
-    mergesort(tempLeft, mid+1, hi);
+    mergesort(data, lo, mid);
+    mergesort(data, mid+1, hi);
   }
 
 }
