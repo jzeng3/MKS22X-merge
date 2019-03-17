cb978740fe1ee75a23eba2c7b0f2a75331a679a7
Jamie Zeng <jzeng3@stuy.edu>
2019-03-16 20:24:38 -0400

    loops through and merges but not sure what to do with merged array

--- Merge.java
+++ Merge.java
@@ -3,20 +3,21 @@ import java.util.*;
 public class Merge{
   public static void main(String[] args){
     int[] ary = {0,1,5,32,7,5,3};
-    int[] arySub1 = {0,1};
+    int[] arySub1 = {1,4,7,9};
     int[] arySub2 = {5,32};
     mergesort(ary, 0, ary.length - 1);
-    int[] merge = sort(arySub1, arySub2);
-    System.out.println(Arrays.toString(merge));
+    System.out.println(Arrays.toString(ary));
+    //int[] merge = sort(arySub1, arySub2);
+    //System.out.println(Arrays.toString(merge));
   }
 
   public static void mergesort(int[]data){
 
   }
   public static void mergesort(int[] data, int lo, int hi){
-    System.out.println("mergesort "+Arrays.toString(data)+", "+lo+", "+hi);
+  //  System.out.println("mergesort "+Arrays.toString(data)+", "+lo+", "+hi);
     if (lo >= hi){
-      System.out.println("return");
+    //  System.out.println("return");
       return;
     }
     int mid = (lo + hi) / 2;
@@ -38,12 +39,16 @@ public class Merge{
     mergesort(data, lo, mid);
     mergesort(data, mid+1, hi);
     System.out.println("after mergesort recursion calls");
-
+    System.out.println("tempLeft: "+Arrays.toString(tempLeft));
+    System.out.println("tempRight: "+Arrays.toString(tempRight));
+    sort(tempLeft,tempRight);
     }
   //  System.out.println("done with everything");
 
 
   public static int[] sort(int[] data1, int[] data2){
+    System.out.println(Arrays.toString(data1));
+    System.out.println(Arrays.toString(data2));
     int[] mergedAry = new int[data1.length + data2.length];
     int d1index = 0; // data1 index
     int d2index = 0; // data2 index
